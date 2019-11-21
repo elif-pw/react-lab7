d76821d54d16de0e21da06be4d542250515baa34
elif-pw <alifozdemir@gmail.com>
2019-11-21 09:34:41 +0100

    Lab7Ex1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, {Component} from 'react'
+import './App.css'
 
-function App() {
-  return (
-    <div>
-      Empty App
-    </div>
-  );
+class App extends Component{
+    constructor(){
+        super();
+        this.state={
+            employees:[]
+        }
+    }
+
+    componentDidMount(){
+        fetch('http://localhost:3004/employees')
+            .then(response=>response.json())
+            .then(data=>this.setState({employees:data}));
+    }
+
+    render(){
+        return(
+            <div>
+                <h1>Employees </h1>
+                {this.state.employees.map(empl=><li className={empl.isActive==true ? "active" : "notactive" } key={empl.id}>{empl.name} {empl.age}</li>)}
+
+            </div>
+        )
+    }
 }
 
 export default App;
