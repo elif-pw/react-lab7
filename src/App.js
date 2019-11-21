fbcc76187a5df2ab6c44fd428e66a337d78387d4
elif-pw <alifozdemir@gmail.com>
2019-11-21 09:58:05 +0100

    Lab7Ex2

--- src/App.js
+++ src/App.js
@@ -5,8 +5,17 @@ class App extends Component{
     constructor(){
         super();
         this.state={
-            employees:[]
+            employees:[],
+
+
+            age:0,
+            name:'',
+            email:'',
+            parentname:'',
+            parentphone:''
+
         }
+        this.onChangeEvent = this.onChangeEvent.bind(this);
     }
 
     componentDidMount(){
@@ -15,12 +24,78 @@ class App extends Component{
             .then(data=>this.setState({employees:data}));
     }
 
+    onChangeEvent(event) {
+        this.setState({[event.target.name]: event.target.value});
+    }
+
+
     render(){
+        const formUnder18 = (
+            <div>
+                <br/>
+                <label>Parent Name    </label>
+                <input
+                    type="email"
+                    name='parentname'
+                    onChange={this.onChangeEvent}
+                    required
+                />
+                <br/><br/>
+                <label>Parent Phone No    </label>
+                <input
+                    type="text"
+                    name='parentphone'
+                    onChange={this.onChangeEvent}
+                    required
+                />
+            </div>
+        );
+        const formAbove18=(
+            <div>
+                <label>Name    </label>
+                <input
+                    type="text"
+                    name='name'
+                    onChange={this.onChangeEvent}
+                    required
+                />
+
+                <label>Email    </label>
+                <input
+                    type="email"
+                    name='email'
+                    onChange={this.onChangeEvent}
+                    required
+                />
+
+            </div>
+
+        );
+
         return(
             <div>
                 <h1>Employees </h1>
                 {this.state.employees.map(empl=><li className={empl.isActive==true ? "active" : "notactive" } key={empl.id}>{empl.name} {empl.age}</li>)}
 
+
+                <form  /*onSubmit={this.handleFormSubmit}*/>
+                    <fieldset>
+                        <h3>Form </h3>
+
+                        <label>Age     </label>
+                        <input
+                            type="number"
+                            name='age'
+                            onChange={this.onChangeEvent}
+                            required
+                        />
+                        <br/>
+                        {this.state.age < 18 ? formUnder18 : formAbove18}
+                        <br/>
+                        <button type="submit">Submit</button>
+                        <br/>
+                    </fieldset>
+                </form>
             </div>
         )
     }
